3aaaa7db8aef22fd2e7f1bc079f3a68b5e0a5a21
unknown <skamta@PC169.BKConsulting.local>
2024-04-18 13:38:20 +0200

    amendment with the specialization of Bernouilli

--- testMathieu.py
+++ testMathieu.py
@@ -11,20 +11,25 @@ class Proba:
 
 
 class ProbaDiscrete(Proba):
-    def __init__(self, values, probabilities):
-        self.state = values
+    def __init__(self, name, state, probabilities):
+        super().__init__(name)
+        self.state = state
         self.probabilities = probabilities
 
     def show_info(self):
-        print("Values :", self.state)
-        print("Probabilities :", self.probabilities)
+        print(self.name)
+        res = []
+        for i in range(len(self.state)):
+            res.append([self.state[i],self.probabilities[i]])
+            print(res[-1])
 
     def __copy__(self):
         return type(self)(self.state.copy(), self.probabilities.copy())
 
 
 class ProbaContinuous(Proba):
-    def __init__(self, density_function):
+    def __init__(self, name,density_function):
+        super().__init__(name)
         self.density_function = density_function
 
     def show_info(self):
@@ -37,28 +42,26 @@ class ProbaContinuous(Proba):
 class Bernouilli(ProbaDiscrete):
      def __init__(self, param):
         if param <0:
-            raise Exception(f"Bernoulli param must be above 0" ({param})")
+            raise Exception(f"Bernoulli param must be above 0 ({param})")
         if param >1:
-            raise Exception(f"Bernoulli param must be below 1" ({param})")
+            raise Exception(f"Bernoulli param must be below 1 ({param})")
+        self.name = type(self)
         self.param = param
         self.state = [0,1]
-        self.probabilities = [param;1-param]
+        self.probabilities = [param,1-param]
 
-    def show_info(self):
-        print("Parameter :", self.param)
-        print("Values :", self.values)
-        print("Probabilities :", self.probabilities)
-
-    def __copy__(self):
+     def __copy__(self):
         return type(self)(self.values.copy(), self.probabilities.copy())
     
 # Exemple d'utilisation
 if __name__ == "__main__":
     # Création d'une distribution de probabilité discrète
-    proba_discrete = ProbaDiscrete([1, 2, 3], [0.2, 0.5, 0.3])
-    proba_discrete_copy = copy.copy(proba_discrete)
+    proba_discrete = ProbaDiscrete("manual Discrete", [1, 2, 3], [0.2, 0.5, 0.3])
+    #proba_discrete_copy = copy.copy(proba_discrete)
     proba_discrete.show_info()
-    proba_discrete_copy.show_info()
+    B = Bernouilli(0.4)
+    B.show_info()
+
 
     # Création d'une distribution de probabilité continue
     def density(x):
@@ -70,7 +73,7 @@ if __name__ == "__main__":
         else:
             return 0.5 * x ** 2
 
-    proba_continuous = ProbaContinuous(density)
-    proba_continuous_copy = copy.copy(proba_continuous)
+    proba_continuous = ProbaContinuous("manual Continue",density)
+    #proba_continuous_copy = copy.copy(proba_continuous)
     proba_continuous.show_info()
-    proba_continuous_copy.show_info()
+    #proba_continuous_copy.show_info()
